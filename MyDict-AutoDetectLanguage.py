8bb5f93acd676eb71fe07404d40158c57982d38b
shadialbatal <shadialbatal1993@hotmail.com>
2019-08-02 01:54:38 +0200

    separated the Dicts to other files to expand it with words freely, and to keep the code clean as possible

--- MyDict-AutoDetectLanguage.py
+++ MyDict-AutoDetectLanguage.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 from Tkinter import *
 from langdetect import detect
-
+import Swedish2Arabic
+import Arabic2Swedish
 
 class Window(Frame):
 
@@ -44,21 +45,6 @@ class Window(Frame):
         self.result= Text(self.master, width=75, height=6, background="white")
         self.result.grid(row=10, column=0, columnspan=1, sticky=W)
 
-        print("LaIlahIlaALLAH")
-        self.ArDict= {
-            'الله':'ALLAH',
-            'باب':'dörr',
-            'كتاب': 'bok'
-        }
-        print("BMLLH")
-
-        self.SwDict= {
-            'GUD': "الله",
-            'dörr': "باب",
-            'bok': "كتاب",
-        }
-        print("LHMDL")
-
     def BsmALLAH(self):
             print("LHKBR")
             textentry= self.textentry.get()
@@ -67,13 +53,13 @@ class Window(Frame):
             d= v.encode('ascii','ignore')
             if d=='ar':
                 try:
-                    Meaning= self.ArDict[textentry]
+                    Meaning= Arabic2Swedish.ArDict[textentry]
                 except:
                     Meaning= "not found"
                 self.result.insert(END, Meaning)
             elif d=='sv':
                 try:
-                    Meaning= self.SwDict[textentry]
+                    Meaning= Swedish2Arabic.SwDict[textentry]
                 except:
                     Meaning= "not found"
                 self.result.insert(END, Meaning)
