2db2ef454fe01688ca7aa78ae30b73443df2ea67
shadialbatal <shadialbatal1993@hotmail.com>
2019-08-02 01:56:01 +0200

    2 files, one for Sv2AR Dict and the other for AR2SV Dict

---
+++ Arabic2Swedish.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+from __future__ import unicode_literals
+
+print("LaIlahIlaALLAH")
+ArDict= {
+'الله':'ALLAH',
+'باب':'dörr',
+'كتاب': 'bok'
+    }
+print("BMLLH")
---
+++ Swedish2Arabic.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+from __future__ import unicode_literals
+
+print("LaIlahIlaALLAH")
+SwDict= {
+'GUD': "الله",
+'dörr': "باب",
+'bok': "كتاب",
+    }
+print("LHMDL")
